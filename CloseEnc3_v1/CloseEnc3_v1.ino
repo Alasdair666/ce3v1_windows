3a71f63e75f2001b258d19f13d24705155859b9e
Alasdair666 <alex-603@hotmail.com>
2014-01-06 16:52:21 +0000

    completed win version

---
+++ CloseEnc3_v1/CloseEnc3_v1.ino
@@ -0,0 +1,54 @@
+    /*
+    1st attempt at close enc organ using RGB LED
+    */
+     
+    int redPin = 11;
+    int greenPin = 10;
+    int bluePin = 9;
+    int usbnumber = 0; //this variable holds what we are currently reading from serial
+     
+    void setup()
+    {
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    Serial.begin(9600); //start up serial port;
+    }
+     
+    void loop()
+    {
+     if (Serial.available() > 0) { //if there is anything on the serial port, read it
+       usbnumber = Serial.read(); //store it in the usbnumber variable
+    }
+    
+     if (usbnumber > 0) {
+         switch(usbnumber){
+                 case 49:  setColor(255, 0, 0); //red - G
+                           tone(8,293);
+                   break;
+                 case 50:  setColor(255, 30, 0);//Orange - A
+                           tone(8,329);
+                   break;
+                 case 51:  setColor(255, 0, 50); //Purple - F
+                           tone(8,130.81);
+                   break;
+                 case 52:  setColor(255, 90, 0); //Yellow - F an octave lower
+                           tone(8,130);
+                   break;
+                 case 53:  setColor(255, 255, 255); // White - C
+                           tone(8,196);
+                   break;
+                 case 54:  setColor(0, 0, 0); // off
+                           noTone(8);
+                   break;
+               }
+    }
+    
+   }
+     
+    void setColor(int red, int green, int blue)
+    {
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
+    }
--- sendNote.php
+++ sendNote.php
@@ -12,34 +12,29 @@ exec("mode COM3: BAUD=9600 PARITY=n DATA=8 STOP=1 to=off dtr=off rts=off");
 
 if (isset($_REQUEST['note'])){
     switch($_REQUEST['note']){
-        case 'Red': //$fp = fopen($comPort, "w");
-            $fp = fopen("COM3", "w");
-            if ( !$fp ) {
-                    print_r(error_get_last());
-                  }
-                    fwrite($fp, 1) or die("Could not write to usb"); /* this is the number that it will write */
-                    //fclose($fp);
-                    //echo "Sending colour: " . $_REQUEST['note'];
+        case 'Red': $fp = fopen("COM3", "w");
+                    fwrite($fp, 1); /* this is the number that it will write */
+                    fclose($fp);
                     break;
         case 'Orange': $fp = fopen("COM3", "w");
                         fwrite($fp, 2); /* this is the number that it will write */
                         fclose($fp);
-                    echo "Sending colour: " . $_REQUEST['note'];
                     break;
         case 'Purple': $fp = fopen("COM3", "w");
                         fwrite($fp, 3); /* this is the number that it will write */
                         fclose($fp);
-                    echo "Sending colour: " . $_REQUEST['note'];
                     break;
         case 'Yellow': $fp = fopen("COM3", "w");
                         fwrite($fp, 4); /* this is the number that it will write */
                         fclose($fp);
-                        echo "Sending colour: " . $_REQUEST['note'];
                     break;
         case 'White': $fp = fopen("COM3", "w");
                         fwrite($fp, 5); /* this is the number that it will write */
                         fclose($fp);
-                        echo "Sending colour: " . $_REQUEST['note'];
+                    break;
+        case 'Stop': $fp = fopen("COM3", "w");
+                        fwrite($fp, 6); /* this is the number that it will write */
+                        fclose($fp);
                     break;
         default: die('The page just puked');
     }
